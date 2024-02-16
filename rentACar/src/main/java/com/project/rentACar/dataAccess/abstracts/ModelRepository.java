bd302ef0c6d8f4190825ae0a18570869f599a067
Kaan Kahveci <kaankahveci29@outlook.com>
2024-02-16 12:06:42 +0300

    Add validation rule for Model

--- rentACar/src/main/java/com/project/rentACar/dataAccess/abstracts/ModelRepository.java
+++ rentACar/src/main/java/com/project/rentACar/dataAccess/abstracts/ModelRepository.java
@@ -4,5 +4,5 @@ import com.project.rentACar.entities.concretes.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ModelRepository extends JpaRepository<Model, Integer> {
-
+    boolean existsByNameAndBrandId(String name, int brandId);
 }
