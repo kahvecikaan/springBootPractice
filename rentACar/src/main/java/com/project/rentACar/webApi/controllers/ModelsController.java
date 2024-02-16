f722074f1bee4640d190c07822a8a1860c0d134a
Kaan Kahveci <kaankahveci29@outlook.com>
2024-02-16 12:07:03 +0300

    Update ModelsController

--- rentACar/src/main/java/com/project/rentACar/webApi/controllers/ModelsController.java
+++ rentACar/src/main/java/com/project/rentACar/webApi/controllers/ModelsController.java
@@ -2,10 +2,14 @@ package com.project.rentACar.webApi.controllers;
 
 import com.project.rentACar.business.abstracts.ModelService;
 import com.project.rentACar.business.requests.CreateModelRequest;
+import com.project.rentACar.business.requests.UpdateModelRequest;
 import com.project.rentACar.business.responses.GetAllModelsResponse;
+import com.project.rentACar.business.responses.GetByIdBrandResponse;
+import com.project.rentACar.business.responses.GetByIdModelResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,13 +21,32 @@ public class ModelsController {
     private ModelService modelService;
 
     @GetMapping()
-    public List<GetAllModelsResponse> getAll() {
-        return modelService.getAll();
+    public ResponseEntity<List<GetAllModelsResponse>> getAll() {
+        var response = modelService.getAll();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping()
-    @ResponseStatus(code = HttpStatus.CREATED) // 201
-    public void add(@RequestBody() @Valid() CreateModelRequest request) {
+    public ResponseEntity<Void> add(@RequestBody() @Valid() CreateModelRequest request) {
         this.modelService.add(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping // put for update
+    public ResponseEntity<Void> update(@RequestBody() @Valid() UpdateModelRequest request) {
+        this.modelService.update(request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") int id) {
+        this.modelService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GetByIdModelResponse> getById(@PathVariable("id") int id) {
+        var response = modelService.getById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
